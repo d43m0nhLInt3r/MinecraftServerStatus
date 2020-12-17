fa4a83a5b0f8f12938a6efea5019b960844507c1
d43m0nhLInt3r <51292471+d43m0nhLInt3r@users.noreply.github.com>
2020-12-17 18:44:44 +0100

    new index.php built with bootstrap

--- index.php
+++ index.php
@@ -2,63 +2,46 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <title>Website</title>
-  <link href="bootstrap.min.css" rel="stylesheet">
-  <script src="bootstrap.min.js"></script>
-  <script src="jquery-3.4.1.slim.min.js"></script>
+    <meta charset="utf-8">
+    <title>Website</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <!-- Brand -->
-    <a class="navbar-brand" href="#">Website</a>
+    <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <a class="h5 navbar-brand my-0 me-md-auto fw-normal" href="index.php">Website</a>
+        <nav class="my-2 my-md-0 me-md-3">
+            <a class="p-2 text-dark" href="mcss/index.php">MCSS</a>
+            <a class="p-2 text-dark" href="#">Link</a>
+        </nav>
+    </header>
+    <main class="container mt-5">
 
-    <!-- Links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="mcss/index.php">Minecraft Server Status</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="openLGSDownloadPage/index.html">Link 1</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Link 2</a>
-      </li>
+        <div class="card mt-5">
+            <div class="card-header">Minecraft Server Status</div>
+            <div class="card-body">A Page where you can check server specific information<a
+                    class="btn btn-large btn-primary" style="float: right;" href="mcss/index.php">visit</a></div>
+        </div>
 
-    </ul>
-  </nav>
-  <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1>Website</h1>
-      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr...</p>
-    </div>
-  </div>
-  <div id="container" style="margin-right: 5%; margin-left: 5%;">
+        <div class="card" style="margin-top: 30px;">
+            <div class="card-header">Link 2</div>
+            <div class="card-body">Lorem ipsum dolor sit amet, consetetur sadipscing elitr...<a
+                    class="btn btn-large btn-primary disabled" style="float: right;" href="#">visit</a></div>
+        </div>
 
-    <div class="card">
-      <div class="card-header">Minecraft Server Status</div>
-      <div class="card-body">A Page where you can check server specific information<a class="btn btn-large btn-info" style="float: right;" href="mcss/index.php">visit</a></div>
-    </div>
-    <div class="card" style="margin-top: 30px;">
-      <div class="card-header">Link 1</div>
-      <div class="card-body">Lorem ipsum dolor sit amet, consetetur sadipscing elitr...<a class="btn btn-large btn-info" style="float: right;" href="openLGSDownloadPage/index.html">visit</a></div>
-    </div>
+        <div class="card" style="margin-top: 30px;">
+            <div class="card-header">Website</div>
+            <div class="card-body">Lorem ipsum dolor sit amet, consetetur sadipscing elitr...<a
+                    class="btn btn-large btn-primary disabled" style="float: right;" href="path/to/file">Download</a>
+            </div>
+        </div>
 
-    <div class="card" style="margin-top: 30px;">
-      <div class="card-header">Link 2</div>
-      <div class="card-body">Lorem ipsum dolor sit amet, consetetur sadipscing elitr...<a class="btn btn-large btn-info disabled" style="float: right;" href="#">visit</a></div>
-    </div>
+        </div>
 
-    <div class="card" style="margin-top: 30px;">
-      <div class="card-header">Website</div>
-      <div class="card-body">Lorem ipsum dolor sit amet, consetetur sadipscing elitr...<a class="btn btn-large btn-info" style="float: right;" href="path/to/file">Download</a></div>
-    </div>
-
-
-  </div>
-
-
+    </main>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 </body>
 
 </html>
