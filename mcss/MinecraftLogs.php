45a405e22f3d2366c31d34a6568f12c4b45171eb
d43m0nhLInt3r <51292471+d43m0nhLInt3r@users.noreply.github.com>
2020-12-21 21:31:29 +0100

    21 instead of 10 lines

--- mcss/MinecraftLogs.php
+++ mcss/MinecraftLogs.php
@@ -8,7 +8,7 @@
  
     <?php
     $log = file("/path/to/MinecraftServer/logs/latest.log");
-    $log = array_slice($log,-10);//the last 10 lines
+    $log = array_slice($log,-21);//the last 21 lines
     foreach($log as $lines) {
 	echo "$lines<br />";
 	}
